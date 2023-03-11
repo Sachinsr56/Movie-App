ad7d2f7133300ca80acbe182844e21281a034437
Sachinsr56 <sachinkumarhas@gmail.com>
2023-03-11 21:24:49 +0530

    removed extra

--- src/Row.js
+++ src/Row.js
@@ -1,28 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from './axios';
+import "./Row.css";
+// import YouTube from "react-youtube";
+// import movieTrailer from "movie-trailer";
 
-function Row({title , fetchUrl}){
-    const [movies, setMovies]= useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/"
 
-    useEffect(() =>{
-            axios
-        .get("https://api.themoviedb.org/3/movie/550?api_key=a5dafd3efcd84e1ba2935d31ea68de6d")
-        .then( (response)=> {
-        console.log(response.data.overview);
-        // setoldmessages(response.data.Message)
-    });
-        },
-        
-     []);
+function Row({ title, fetchUrl, isLargeRow }) {
+    const [movies, setMovies] = useState([]);
+    // const [trailerUrl, setTrailerUrl] = useState("");
 
-    console.log(movies);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
+
+    // const opts = {
+    //     height: "390",
+    //     width: "100%",
+    //     playerVars: {
+    //         autoplay: 1,
+    //     },
+    // };
+    // const handleClick = (movie) => {
+    //     if (trailerUrl) {
+    //         setTrailerUrl('');
+    //     }
+    //     else {
+    //         movieTrailer(movie?.name || "")
+    //             .then((url) => {
+    //                 const urlParams = new URLSearchParams(URL(url).search);
+    //                 setTrailerUrl(urlParams.get("v"));
+    //             })
+    //             .catch((error) => console.log(error));
+    //     }
+    // }
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
+            <div className="row_posters">
 
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        // onClick={() => handleClick(movie)}
+                        className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                        src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                        alt={movie.name}
+                    />
+                ))}
 
+            </div>
+
+            {/* {trailerUrl && <YouTube videoId="X4bF_quwNtw" opts={opts} />} */}
         </div>
     )
 }
--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,7 @@
-// // import axios from "./axios";
-// // base url to make requests to the movie database
+import axios from "axios";
 
-// const instance=axios.create({
-//     baseURL:"https://api.themovie.org/3",
-// });
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+})
 
-// instance.get('/foo-bar');
-
-// export default instance;
-
+export default instance;
